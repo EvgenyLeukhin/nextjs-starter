2c46fd3b081d6ea2fbe858c85e3d6d8bc7fd0c30
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-02-05 13:38:28 +0600

    Update

--- next.config.js
+++ next.config.js
@@ -5,18 +5,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '/';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   sassOptions: {
@@ -26,13 +14,6 @@ const nextConfig = withBundleAnalyzer({
   webpack: config => {
     config.plugins.push(new CompressionPlugin());
     return config;
-  },
-
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
   },
 });
 
