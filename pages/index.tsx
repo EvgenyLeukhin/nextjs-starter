43b64a96eed3c4cbf340ca8643bb408cd3c90c81
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-02-05 14:11:21 +0600

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Container>
-        <h1 className='text-primary'>Home page+++</h1>
+        <h1 className='text-primary'>Home page</h1>
 
         <ToDoList />
       </Container>
