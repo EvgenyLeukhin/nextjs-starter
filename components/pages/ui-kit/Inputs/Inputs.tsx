c17b5be70641eed062348613c2dbbefad58e6f6a
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-02-27 20:44:48 +0600

    Update

--- components/pages/ui-kit/Inputs/Inputs.tsx
+++ components/pages/ui-kit/Inputs/Inputs.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Input, RadioGroup, Select } from '@/components/ui';
+import { Checkbox, Input, RadioGroup, Select, File } from '@/components/ui';
 import { InputList } from '@/types/common';
 import styles from './Inputs.module.scss';
 
@@ -90,6 +90,16 @@ const Inputs = () => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setFieldValue={(field, value, shouldValidate) => {
           return null;
+        }}
+      />
+
+      <File
+        label='File'
+        id='test-file'
+        name='test-file'
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        setFieldValue={function (field, value, shouldValidate): void {
+          null;
         }}
       />
 
