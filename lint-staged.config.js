3727482412db8d895c35e4ce8e5f8ff25424d287
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-03-05 19:52:32 +0600

    Update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,23 @@
 module.exports = {
+  // // this will check Typescript files
+  // '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // // This will lint and format TypeScript and JavaScript files
+  // '**/*.(ts|tsx|js)': filenames => [
+  //   `yarn eslint --fix ${filenames.join(' ')}`,
+  //   `yarn prettier --write ${filenames.join(' ')}`,
+  // ],
+
+  // // this will Format MarkDown and JSON
+  // '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+
+  // // scss files
+  // '**/*.(scss)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+
   // this will check Typescript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
-  // This will lint and format TypeScript and JavaScript files
+  // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': filenames => [
     `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
@@ -10,7 +25,4 @@ module.exports = {
 
   // this will Format MarkDown and JSON
   '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
-
-  // scss files
-  '**/*.(scss)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
 };
