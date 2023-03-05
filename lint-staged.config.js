bc810b39154fc41601b642c0e0f8bbe4c84eaec1
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-03-05 19:56:04 +0600

    Update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,19 +1,4 @@
 module.exports = {
-  // // this will check Typescript files
-  // '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
-  // // This will lint and format TypeScript and JavaScript files
-  // '**/*.(ts|tsx|js)': filenames => [
-  //   `yarn eslint --fix ${filenames.join(' ')}`,
-  //   `yarn prettier --write ${filenames.join(' ')}`,
-  // ],
-
-  // // this will Format MarkDown and JSON
-  // '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
-
-  // // scss files
-  // '**/*.(scss)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
-
   // this will check Typescript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
@@ -25,4 +10,7 @@ module.exports = {
 
   // this will Format MarkDown and JSON
   '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
+
+  // scss files
+  '**/*.(scss)': filenames => `yarn prettier --write ${filenames.join(' ')}`,
 };
