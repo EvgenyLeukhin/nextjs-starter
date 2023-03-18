42ed9780041e687ff83c4d9208c31fa5d98b33dc
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-03-18 12:36:40 +0600

    Update

--- components/ui/ReactTooltip/ReactTooltip.tsx
+++ components/ui/ReactTooltip/ReactTooltip.tsx
@@ -1,26 +1,10 @@
-import { Tooltip, VariantType } from 'react-tooltip';
-import classNames from 'classnames/bind';
-import styles from './ReactTooltip.module.scss';
-
 type TProps = {
-  variant?: VariantType;
   text: string;
-  anchor: string;
-  children: React.ReactNode;
+  // children: React.ReactNode;
 };
 
-const ReactTooltip = ({ text, variant = 'info', anchor, children }: TProps) => {
-  const cnb = classNames.bind(styles);
-
-  return (
-    <>
-      <span data-tooltip-content={text} className={cnb(styles.Tooltip, anchor)}>
-        {children}
-      </span>
-
-      <Tooltip anchorSelect={`.${anchor}`} variant={variant} />
-    </>
-  );
+const ReactTooltip = ({ text }: TProps) => {
+  return <div>ReactTooltip {text}</div>;
 };
 
 export default ReactTooltip;
