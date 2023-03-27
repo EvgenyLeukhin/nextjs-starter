7a33fea1b6697e1876ae53d1a09f6137aef89cef
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-03-27 17:38:35 +0600

    Update

--- components/pages/rest-api/TableExample/TableExample.tsx
+++ components/pages/rest-api/TableExample/TableExample.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import styles from './TableExample.module.scss';
 import { getUsers } from '@/api/servicies';
 import { TUser } from '@/types/user';
 import { Loader } from '@/components/ui';
+import styles from './TableExample.module.scss';
 
 const TableExample = () => {
   const [data, setData] = useState([]);
@@ -22,40 +22,61 @@ const TableExample = () => {
     });
   }, []);
 
-  // console.log('data', data);
-
+  // returnData
   const returnData = () => {
-    return data.map((user: TUser) => {
+    return data.map((user: TUser, index: number) => {
       const { id, name, surname, email, company, location } = user;
 
+      // userData object
+      const userData = {
+        id,
+        name,
+        email,
+        company,
+        location,
+      };
+
+      // userData keys to map headers
+      const userDataKeys = Object.keys(userData);
+
       return (
-        <tr key={id}>
-          <td>{id}</td>
-          <td>{`${name} ${surname}`}</td>
-          <td>{email}</td>
-          <td
-            style={{
-              display: 'inline-flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            {company?.logo && (
-              <>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={company?.logo}
-                  alt={company?.name}
-                  width={20}
-                  height='auto'
-                />
-                &nbsp;
-              </>
-            )}
-            <span>{company?.name}</span>
-          </td>
-          <td>{`${location?.name}, ${location?.country}`}</td>
-        </tr>
+        <>
+          {index === 0 && (
+            <tr key={id} style={{ textAlign: 'left' }}>
+              {userDataKeys.map((key, index) => {
+                return <th key={index}>{key}</th>;
+              })}
+            </tr>
+          )}
+
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{`${name} ${surname}`}</td>
+            <td>{email}</td>
+            <td
+              style={{
+                display: 'inline-flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {company?.logo && (
+                <>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={company?.logo}
+                    alt={company?.name}
+                    width={20}
+                    height='auto'
+                  />
+                  &nbsp;
+                </>
+              )}
+              <span>{company?.name}</span>
+            </td>
+            <td>{`${location?.name}, ${location?.country}`}</td>
+          </tr>
+        </>
       );
     });
   };
