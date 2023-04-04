61396f9d6e79c015f085791cd0741219f843ed50
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2023-04-04 22:23:16 +0600

    Add redux-classic

--- components/pages/rest-api/ReduxClassicExample/ReduxClassicExample.tsx
+++ components/pages/rest-api/ReduxClassicExample/ReduxClassicExample.tsx
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from './ReduxClassicExample.module.scss';
 import { AppState } from '@/store-classic/types/AppState';
 import { UserType } from '@/store-classic/types/User';
-import { TodoType } from '@/store-classic/types/Todo';
+// import { TodoType } from '@/store-classic/types/Todo';
 import useEffectOnce from '@/utils/hooks/useEffectOnce';
 import { fetchTodosThunk } from '@/store-classic/todos/action-creators';
 import { fetchUsersThunk } from '@/store-classic/users/action-creators';
+import { Loader } from '@/components/ui';
 
 // redux
 // redux-thunk
@@ -19,19 +20,45 @@ const ReduxClassicExample = () => {
     dispatch(fetchUsersThunk(10) as any);
   });
 
-  const todos: Array<TodoType> = useSelector(
-    (state: AppState) => state.todos.todoList,
-  );
-  const users: Array<UserType> = useSelector(
-    (state: AppState) => state.users.userList,
-  );
+  // const todos: Array<TodoType> = useSelector((state: AppState) => {
+  //   return state.todos.todoList;
+  // });
 
-  console.log('todos', todos);
-  console.log('users', users);
+  // get users state from the store
+  const users = useSelector((state: AppState) => state.users);
+
+  const { isFetching, error, userList } = users;
+
+  const returnUsers = () => {
+    // loading
+    if (isFetching) {
+      return <Loader />;
+    }
+
+    // error
+    if (error) {
+      return <span>{`${error}`}</span>;
+    }
+
+    // get data
+    if (userList.length) {
+      return userList.map((user: UserType) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <a href={`mailto:${user.email}`}>{user.email}</a>
+          <i>{user.username}</i>
+        </div>
+      ));
+    } else return 'No data';
+  };
 
   return (
     <section className={styles.ReduxClassicExample}>
       <h2>Redux Classic Example</h2>
+
+      <div className={styles.ReduxClassicExample__userList}>
+        {returnUsers()}
+      </div>
     </section>
   );
 };
